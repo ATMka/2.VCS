ae5f4e301fa5567d74f9ad7d02fc04d73e29c993
atmka <corp.dvd@gmail.com>
2019-06-23 14:32:23 +0400

    Добавлен второй класс, в FirstClass создан экземпляр SecondClass

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -2,4 +2,7 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello");
     }
+
+    SecondClass Class2 = new SecondClass(10,5);
+
 }
---
+++ src/SecondClass.java
@@ -0,0 +1,35 @@
+public class SecondClass {
+    private int one;
+    private int two;
+    private String command="Тест";
+
+    public SecondClass(int one, int two) {
+        this.one = one;
+        this.two = two;
+    }
+
+
+    public int getOne() {
+        return one;
+    }
+
+    public void setOne(int one) {
+        this.one = one;
+    }
+
+    public int getTwo() {
+        return two;
+    }
+
+    public void setTwo(int two) {
+        this.two = two;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+}

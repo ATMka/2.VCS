f19af0408202cb4e3842d603dff8bb9266eb53dc
Alexey Byanov <corp.dvd@gmail.com>
2019-06-24 11:21:21 +0400

    Update FirstClass.java

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -3,6 +3,6 @@ public class FirstClass {
     public static void main(String[] args) {
         SecondClass Class2 = new SecondClass(10,5);
         System.out.println("Hello");
-        System.out.println("Мы тут вообще-то тут домашнее задание делаем. Вот смотрите, переменная one из Class2 - " + Class2.getOne());
+        System.out.println("Мы тут вообще-то домашнее задание делаем. Вот смотрите, переменная one из Class2 - " + Class2.getOne());
     }
 }

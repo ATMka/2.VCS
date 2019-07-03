f93f29fbb2547e9771972ae312f6009dc5d5026f
atmka <226408>
2019-07-03 11:12:37 +0400

    Созданы переменные класса Animals + 1 экземпляр класса + интерфейс.

--- src/Animals.java
+++ src/Animals.java
@@ -1,2 +1,39 @@
-public class Animals {
+public class Animals implements AnimalsImpl {
+    public Animals(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public Animals() {
+    }
+
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Animals{" +
+                "age=" + age +
+                '}';
+    }
+    @Override
+    public void runForRoad(){
+        System.out.println(getName() +  " очень быстро бежит по дороге, хотя этому животному уже " + getAge() + " лет!");
+    }
 }
--- src/FirstClass.java
+++ src/FirstClass.java
@@ -5,5 +5,7 @@ public class FirstClass {
         System.out.println("Hello");
         System.out.println("Мы тут вообще-то домашнее задание делаем. Вот смотрите, переменная one из Class2 - " + Class2.getOne());
         System.out.println("Здесь еще должен был быть экземпляр класса Animals, но в master его нет.");
+        Animals dog = new Animals(29, "Бобик");
+        dog.runForRoad();
     }
 }
